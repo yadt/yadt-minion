ac5e9638c169ba7c87b82e27492b82e52123c9d3
Arne Hilmann <arne@hilmann.de>
2013-07-17 14:52:14 +0200

    adding old default file for smooth rollout of the new yadt config

--- build.py
+++ build.py
@@ -55,6 +55,7 @@ def set_properties(project):
     project.set_property('dir_dist_scripts', 'scripts')
 
     project.install_file('/etc/yadt.conf.d/', 'yadtminion/00_defaults')
+    project.install_file('/etc/default/', 'yadtminion/yadt')
 
     project.get_property('distutils_commands').append('bdist_rpm')
     project.set_property('distutils_classifiers', [
