d025ee280a43989c6c4c0711dc7ff4d4dff455c5
Stefan Nordhausen <stefan.nordhausen@immobilienscout24.de>
2015-06-25 11:23:30 +0200

    Make it easier to start developing

---
+++ setup_local_environment.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Setup script for Ubuntu/Debian or Red Hat/Clones
+# Run this program to get an initial setup that allows you to run unit+integration tests.
+
+# Add required packages as space seperated list:
+DEBIAN_PACKAGES=yum
+REDHAT_PACKAGES=yum
+
+# Add additional virtualenv options:
+VIRTUALENV_OPTIONS="--system-site-packages"
+
+if type apt-get &>/dev/null ; then
+    dpkg -s $DEBIAN_PACKAGES python-virtualenv &>/dev/null || sudo apt-get install $DEBIAN_PACKAGES python-virtualenv
+else
+    rpm -q $REDHAT_PACKAGES python-virtualenv &>/dev/null || sudo yum install $REDHAT_PACKAGES python-virtualenv
+fi
+
+virtualenv $VIRTUALENV_OPTIONS venv
+
+# Enter the virtual environment before "pip install ...", so that things get
+# installed in venv, not in the main system.
+source venv/bin/activate
+pip install pybuilder
+pyb install_dependencies
