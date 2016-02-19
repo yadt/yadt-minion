ed8c9b518004bc3e41791973832797c6d3a1ea02
Tobias Vollmer <tobias.vollmer@immobilienscout24.de>
2016-02-19 16:00:11 +0100

    PIO-170: Rename variables

--- src/main/python/yadtminionutils/yadtminionutils.py
+++ src/main/python/yadtminionutils/yadtminionutils.py
@@ -1,9 +1,11 @@
 import os
 
 
-def get_files_by_template(common_name, list_of_templates):
-    existing_file_list = []
-    for file_to_test in [f.format(common_name) for f in list_of_templates]:
+def get_files_by_template(common_name, filename_templates):
+    existing_files = []
+    files_to_test = [template.format(common_name)
+                     for template in filename_templates]
+    for file_to_test in files_to_test:
         if os.path.exists(file_to_test):
-            existing_file_list.append(file_to_test)
-    return existing_file_list
+            existing_files.append(file_to_test)
+    return existing_files
