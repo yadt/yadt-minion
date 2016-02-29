159d6c08341f6990627c3c180dcfc8b19cc8ada2
Tobias Vollmer <tobias.vollmer@immobilienscout24.de>
2016-02-29 10:12:40 +0100

    Add Handling of xinetd services

--- src/integrationtest/python/yadtminionutils_tests.py
+++ src/integrationtest/python/yadtminionutils_tests.py
@@ -79,6 +79,25 @@ class Test(unittest.TestCase):
         chkconfigmock.assert_called_once_with()
         self.assertFalse(is_sysv_service("noservice"))
 
+    @patch("yadtminionutils.sysv_scripts.Command")
+    def test_is_sysv_service_should_return_correct_value_with_xinetd(self, command):
+        chkconfigmock = Mock()
+        chkconfigmock.return_value = dedent("""
+            service1    	0:off   1:off   2:on    3:on    4:on    5:on    6:off
+            service2    	0:off   1:off   2:on    3:on    4:on    5:on    6:off
+            ser3           	0:off   1:off   2:off   3:on    4:on    5:on    6:off
+            service4    	0:off   1:off   2:on    3:on    4:on    5:on    6:off
+            longservice5	0:off   1:off   2:off   3:on    4:on    5:on    6:off
+            service6    	0:off   1:on    2:on    3:on    4:on    5:on    6:off
+
+            xinetd based services:
+                foobar-server:  on
+            """).strip().split("\n")
+        command.return_value = chkconfigmock
+        self.assertTrue(is_sysv_service("service1"))
+        chkconfigmock.assert_called_once_with()
+        self.assertFalse(is_sysv_service("noservice"))
+
 
 if __name__ == "__main__":
     unittest.main()
--- src/main/python/yadtminionutils/sysv_scripts.py
+++ src/main/python/yadtminionutils/sysv_scripts.py
@@ -3,5 +3,11 @@ from sh import Command
 
 def is_sysv_service(service_name):
     chkconfig = Command("/sbin/chkconfig")
-    sysv_services = [line.split()[0] for line in chkconfig()]
+    sysv_services = []
+    for line in chkconfig():
+        line = line.strip()
+        if not line:
+            # Empty line is the start of the "xinetd based services" section.
+            break
+        sysv_services.append(line.split()[0])
     return service_name in sysv_services
